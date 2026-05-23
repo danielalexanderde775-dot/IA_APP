75fd6fda466fff8bd5a17232b64018def171b7b9
Daniel <danielalexanderde775@gmail.com>
2026-05-23 17:00:08 -0600

    Actualización

--- api.py
+++ api.py
@@ -2,58 +2,42 @@ from flask import Flask, request, jsonify
 import joblib
 import numpy as np
 
-from motion_detector import detectar_movimiento
-
 # ---------------- APP ----------------
 app = Flask(__name__)
 
 # ---------------- CARGAR IA ----------------
+# Asegúrate de que estos archivos estén en la misma carpeta que api.py
 model = joblib.load("lsm_model.pkl")
 scaler = joblib.load("scaler.pkl")
 
 # ---------------- RUTA PRINCIPAL ----------------
 @app.route("/")
 def home():
-    return "Servidor IA funcionando"
+    return "Servidor IA de Signavis Funcionando"
 
-# ---------------- PREDICCIÓN ----------------
+# ---------------- PREDICCIÓN ESTÁTICA ----------------
 @app.route("/predict", methods=["POST"])
 def predict():
-
     try:
-        # Recibir datos
+        # Recibir los 63 puntos (21 landmarks x 3 coordenadas)
         data = request.json["data"]
-
-        # Convertir a numpy
+        
+        # Convertir y procesar
         data_np = np.array(data).reshape(1, -1)
-
-        # -------- DETECTOR DE MOVIMIENTO --------
-        # Coordenadas dedo índice
-        x = data[8]
-        y = data[9]
-
-        movimiento = detectar_movimiento(x, y)
-
-        # Si detecta J o Z
-        if movimiento:
-            return jsonify({
-                "prediction": movimiento
-            })
-
-        # -------- IA NORMAL --------
         data_scaled = scaler.transform(data_np)
-
+        
+        # Predecir letra
         prediction = model.predict(data_scaled)
-
+        
         return jsonify({
-            "prediction": prediction[0]
+            "prediction": str(prediction[0])
         })
 
     except Exception as e:
+        # Esto enviará el error directamente a la pantalla de tu celular
         return jsonify({
             "error": str(e)
         })
 
-# ---------------- EJECUTAR ----------------
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
