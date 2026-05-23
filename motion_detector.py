645bdb184d132ec85e2ef9c6eac79a03f7011644
Daniel <danielalexanderde775@gmail.com>
2026-05-23 15:48:14 -0600

    Corrigiendo deteccion de trayectoria

--- motion_detector.py
+++ motion_detector.py
@@ -1,11 +1,29 @@
 trayectoria = []
 
-def detectar_movimiento(x, y):
+def detectar_movimiento(data):
+
     global trayectoria
 
-    trayectoria.append((x, y))
+    # -------- PUNTOS MANO --------
+    indice_tip_x = data[24]
+    indice_tip_y = data[25]
 
-    # Mantener historial
+    indice_base_y = data[19]
+
+    # =========================
+    # SOLO FUNCIONA SI EL ÍNDICE
+    # ESTÁ LEVANTADO
+    # =========================
+    indice_arriba = indice_tip_y < indice_base_y
+
+    if not indice_arriba:
+        trayectoria.clear()
+        return None
+
+    # Guardar trayectoria
+    trayectoria.append((indice_tip_x, indice_tip_y))
+
+    # Limitar historial
     if len(trayectoria) > 20:
         trayectoria.pop(0)
 
@@ -18,17 +36,15 @@ def detectar_movimiento(x, y):
     dy = trayectoria[-1][1] - trayectoria[0][1]
 
     # =========================
-    # DETECCIÓN J
+    # LETRA J
     # =========================
-    # Movimiento hacia abajo + curva leve
     if dy > 0.15 and abs(dx) > 0.08:
         trayectoria.clear()
         return "J"
 
     # =========================
-    # DETECCIÓN Z
+    # LETRA Z
     # =========================
-
     cambios = 0
 
     for i in range(2, len(trayectoria)-2):
@@ -37,13 +53,11 @@ def detectar_movimiento(x, y):
         x2 = trayectoria[i][0]
         x3 = trayectoria[i+1][0]
 
-        # Detectar cambio REAL de dirección
         if (x2 > x1 and x2 > x3) or \
            (x2 < x1 and x2 < x3):
             cambios += 1
 
-    # Necesita MUCHOS cambios
-    if cambios >= 4 and abs(dx) > 0.20:
+    if cambios >= 4 and abs(dx) > 0.25:
         trayectoria.clear()
         return "Z"
 
